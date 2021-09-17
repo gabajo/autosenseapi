544b3e984f1106ab535a58005b85a74d47b20fe5
Marco <asd@lamiamail.ch>
2021-09-17 19:13:06 +0200

    methods

--- src/items/item.interface.ts
+++ src/items/item.interface.ts
@@ -1,10 +1,16 @@
 export interface BaseItem {
     name: string;
+    address: string;
+    city: string;
+    latitude: number;
+    longitude: number;
+    prices: Array<{ price: number, currency: string, product_id: string }>;
+    products: Array<{ product_id: string, points: Array<{ id: string, status: string }> }>
     price: number;
     description: string;
     image: string;
 }
 
 export interface Item extends BaseItem {
-    id: number;
+    id: string;
 }
--- src/items/items.router.ts
+++ src/items/items.router.ts
@@ -12,6 +12,45 @@ itemsRouter.get("/", async (req: Request, res: Response) => {
 
         res.status(200).send(items);
     } catch (e) {
-        res.status(500).send(e.message);
+        res.status(500).send(e);
+    }
+});
+
+
+itemsRouter.get("/:id", async (req: Request, res: Response) => {
+    const id: number = parseInt(req.params.id, 10);
+
+    try {
+        const item: Item = await ItemService.find(id);
+
+        if (item) {
+            return res.status(200).send(item);
+        }
+
+        res.status(404).send("item not found");
+    } catch (e) {
+        res.status(500).send(e);
+    }
+});
+
+
+itemsRouter.put("/:id", async (req: Request, res: Response) => {
+    const id: number = parseInt(req.params.id, 10);
+
+    try {
+        const itemUpdate: Item = req.body;
+
+        const existingItem: Item = await ItemService.find(id);
+
+        if (existingItem) {
+            const updatedItem = await ItemService.update(id, itemUpdate);
+            return res.status(200).json(updatedItem);
+        }
+
+        const newItem = await ItemService.create(itemUpdate);
+
+        res.status(201).json(newItem);
+    } catch (e) {
+        res.status(500).send(e);
     }
 });
